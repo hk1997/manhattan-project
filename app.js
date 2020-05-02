b1a033ee43e95187747f9417f7e88cf9de294943
hk1997 <khandelwal.hardik14@gmail.com>
2020-05-02 20:16:01 +0530

    meeting routes added

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const clientRoutes = require("./routes/clientRoutes");
 const doctorRoutes = require("./routes/doctorRoutes");
 const staffRoutes = require("./routes/staffRoutes");
 const utilityRoutes = require("./routes/utilityRoutes");
+const meetingRoutes = require('./routes/meetingRoutes')
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -32,6 +33,7 @@ app.use("/client", clientRoutes);
 app.use("/doctor", doctorRoutes);
 app.use("/staff", staffRoutes);
 app.use("/utility", utilityRoutes);
+app.use('/meeting',meetingRoutes)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

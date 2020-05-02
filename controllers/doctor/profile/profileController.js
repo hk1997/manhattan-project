6c29b4f1082f9aa2af5fc0e47433695ca54708df
bipinpathak <bipinpathak1411@gmail.com>
2020-05-03 01:14:20 +0530

    costInfo and paymentInfo CRUD. costInfo model updated.

--- controllers/doctor/profile/profileController.js
+++ controllers/doctor/profile/profileController.js
@@ -59,3 +59,133 @@ module.exports.listProfile = async (req, res) => {
         });
     }
 };
+
+module.exports.updatePaymentInfo = async (req, res) => {
+    try {
+        let fields = ["accountNo", "ifscCode"];
+        let modelData = await helpers.initializeData(fields, req);
+        if (!modelData.valid) {
+            return res.json({
+                success: false,
+                message: 'necessary parameters missing',
+                data: {}
+            });
+        }
+        let response = await profileModelController.updatePaymentInfo(req.payload._id, modelData.data);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
+
+module.exports.getPaymentInfo = async (req, res) => {
+    try {
+        let response = await profileModelController.getPaymentInfo(req.payload._id);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
+
+module.exports.deletePaymentInfo = async (req, res) => {
+    try {
+        let response = await profileModelController.deletePaymentInfo(req.payload._id);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
+
+module.exports.listPaymentInfo = async (req, res) => {
+    try {
+        let response = await profileModelController.listPaymentInfo();
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
+
+module.exports.updateCostInfo = async (req, res) => {
+    try {
+        let fields = ["virtualCall", "physicalExamination", "emergencyCharges"];
+        let modelData = await helpers.initializeData(fields, req);
+        if (!modelData.valid) {
+            return res.json({
+                success: false,
+                message: 'necessary parameters missing',
+                data: {}
+            });
+        }
+        let response = await profileModelController.updateCostInfo(req.payload._id, modelData.data);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
+
+module.exports.getCostInfo = async (req, res) => {
+    try {
+        let response = await profileModelController.getCostInfo(req.payload._id);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
+
+module.exports.deleteCostInfo = async (req, res) => {
+    try {
+        let response = await profileModelController.deleteCostInfo(req.payload._id);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
+
+module.exports.listCostInfo = async (req, res) => {
+    try {
+        let response = await profileModelController.listCostInfo();
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            success: false,
+            message: 'some error occured',
+            data: {}
+        });
+    }
+};
